2da3585dfc5929424343f02579861f6a4739a33e
HanHaSub <36252002+HanHaSub@users.noreply.github.com>
2018-03-06 01:18:36 -0600

    new

---
+++ Capitulo 6/Programming Excersise/Excercise 6/EverySum/src/everysum/EverySum.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package everysum;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author acer
+ */
+public class EverySum {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada=new Scanner(System.in);
+        System.out.print("Ingrese el valor final:");
+        int num=entrada.nextInt();
+        int factorial=1;
+        int i=1;
+        for(i=factorial;i>0;i++){
+            System.out.println(i);
+        }
+    }
+}
